19ef1c2c84d2d3004088ca6215f9e66c58539911
Toby Simpson <toby.simpson@usi.ch>
2022-01-24 13:58:58 +0100

    Initial Commit

---
+++ cl_info1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cl_info1
+//
+//  Created by Toby Simpson on 24.01.22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
